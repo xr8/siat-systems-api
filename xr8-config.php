8bcc4527ef6ed4aab967d1030bee4e362467c05b
XR8 - Hp Lap <biohizard@hotmail.com>
2026-03-24 17:32:11 -0600

    update config
    update config

--- xr8-config.php
+++ xr8-config.php
@@ -36,72 +36,80 @@
  * @filesource
  */
 
-// Definir constantes globales***
-define("NAME", "APP SYSTEMS");
-
 // Establecer la zona horaria predeterminada
 date_default_timezone_set('America/Mexico_City');
 
-  // Verificar que la zona horaria se haya establecido correctamente
-  if (date_default_timezone_get() !== 'America/Mexico_City') {
-      // Manejar el error de configuración de zona horaria si es necesario
-      error_log('No se pudo establecer la zona horaria a America/Mexico_City');
-      error_log('Fecha/hora actual: ', date('Y-m-d h:i:s', time()));
-  }
-
-// Dividir el host en partes utilizando el punto como delimitador
-$a_ngrok = explode('.', $_SERVER['HTTP_HOST']);
-
-// Determinar la zona y el título de la página
-function setZoneAndTitle($host, $a_ngrok = []) {
-    if ($host == 'localhost') {
-        define("ZONA", 'local');
-        define("PAGETITLE", 'Local : ');
-    } elseif (!empty($a_ngrok) && isset($a_ngrok[1]) && $a_ngrok[1] == 'ngrok') {
-        define("ZONA", 'ngrok');
-        define("PAGETITLE", 'Local Ngrok: ');
-    } else {
-        define("ZONA", 'web');
-        define("PAGETITLE", 'Remote : ');
-    }
+// Verificar que la zona horaria se haya establecido correctamente
+if (date_default_timezone_get() !== 'America/Mexico_City') {
+  // Manejar el error de configuración de zona horaria si es necesario
+  error_log('No se pudo establecer la zona horaria a America/Mexico_City');
+  error_log('Fecha/hora actual: ', date('Y-m-d h:i:s', time()));
 }
 
-// Llamar a la función con los parámetros adecuados
-setZoneAndTitle($_SERVER['HTTP_HOST'], $a_ngrok);
-
+// Determinar la zona y el título de la página
+function setZoneAndTitle($host, $a_ngrok = [])
+{
+  if ($host == 'localhost') {
+    define("ZONA", 'local');
+    define("PAGETITLE", 'Local : ');
+  } elseif (!empty($a_ngrok) && isset($a_ngrok[1]) && $a_ngrok[1] == 'ngrok') {
+    define("ZONA", 'ngrok');
+    define("PAGETITLE", 'Local Ngrok: ');
+  } else {
+    define("ZONA", 'web');
+    define("PAGETITLE", 'Remote : ');
+  }
+}
 // Configuración basada en la zona
 function configureByZone($zona)
 {
+  if ($zona == 'local') {
 
-    define("TITLE", PAGETITLE . "JM SIAT Systems v1 - ");
-    //JM 107.180.115.28
+    //JM LOCALHOST
+    define("HOSTNAME", 'localhost');
+    define("USERNAME", 'root');
+    define("PASSWORD", "aeiou12345");
+    define("DATABASE", 'siat_db');
+
+    define("BASE_URL",   '//'       . $_SERVER['HTTP_HOST'] . '/server/php/siat-gob-mx-gobcom-mx/siat.gob.mx.gobcom.mx/siat.gob.mx.gobcom.mx/dashboard/');
+    define("APP_URL",    BASE_URL   . "siat-systems-app/");
+    define("API_URL",    BASE_URL   . "siat-systems-api/");
+    define("CDN_URL",    BASE_URL   . "siat-systems-cdn/siat-CDN-app/");
+    define("INDEX_PAGE", APP_URL    . "index.php/");
+
+  } elseif ($zona == 'ngrok') {
+    // Configuración para ngrok
+    echo "ngrok";
+  } elseif ($zona == 'web') {
+    
+    //JM WEB
     define("HOSTNAME", '107.180.115.28');
     define("USERNAME", 'sistema_db');
     define("PASSWORD", "&);ud!.DGP^R5*v");
     define("DATABASE", 'siat_db');
 
-    define("DEFAULTROUTER", 'api/');
+    define("BASE_URL",   '//'       . $_SERVER['HTTP_HOST'] . '/server/php/siat-gob-mx-gobcom-mx/siat.gob.mx.gobcom.mx/siat.gob.mx.gobcom.mx/dashboard/');
+    define("APP_URL",    BASE_URL   . "siat-systems-app/");
+    define("API_URL",    BASE_URL   . "siat-systems-api/");
+    define("CDN_URL",    BASE_URL   . "siat-systems-cdn/siat-CDN-app/");
+    define("INDEX_PAGE", APP_URL    . "index.php/");
 
-    if ($zona == 'local') {
-        // Configuración para local
-        define("BASE_URL", '//'      . $_SERVER['HTTP_HOST'] . '/server/php/siat-gob-mx-gobcom-mx/siat.gob.mx.gobcom.mx/siat.gob.mx.gobcom.mx/dashboard/');
-        define("APP_URL", BASE_URL   . "siat-systems-app/");
-        define("API_URL", BASE_URL   . "siat-systems-api/");
-        define("CDN_URL", BASE_URL   . "siat-systems-cdn/siat-CDN-app/");
-        define("INDEX_PAGE", APP_URL . "index.php/");
-    } elseif ($zona == 'ngrok') {
-        // Configuración para ngrok
-        echo "ngrok";
-    } elseif ($zona == 'web') {
-        // Configuración para web
-        //siatgobcom.mx JM
-        define("BASE_URL", 'siatgobcom.mx/');
-        define("APP_URL", "//app." . BASE_URL);
-        define("API_URL", "//api." . BASE_URL);
-        define("CDN_URL", "//cdn." . BASE_URL ."siat-CDN-app/");
-        define("INDEX_PAGE", APP_URL . '');
-    }
+  }
 }
 
+### Begin: setZoneAndTitle ###
+  // Dividir el host en partes utilizando el punto como delimitador
+  $a_ngrok = explode('.', $_SERVER['HTTP_HOST']);
+  // Llamar a la función con los parámetros adecuados
+  setZoneAndTitle($_SERVER['HTTP_HOST'], $a_ngrok);
+### End: setZoneAndTitle ###
+
+### Begin: configureByZone ###
 // Aplicar la configuración basada en la zona
 configureByZone(ZONA);
+### End: configureByZone ###
+
+// Definir constantes globales***
+define("NAME",          "APP SYSTEMS");
+define("DEFAULTROUTER", 'login/sign_in');
+define("TITLE",         PAGETITLE . "JM SIAT Systems v1 - ");
